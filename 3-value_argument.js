ca3f171237ddde533410906a84f15860b7b0bc6b
OwusuAsiedu <nabcoerdig02594@gmail.com>
2025-12-05 11:47:35 +0000

    3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,8 +1,7 @@
-#!/usr/bin/node
-const argument = process.argv[2];
+const firstArg = process.argv[2];
 
-if (argument === undefined) {
-  console.log('No argument');
+if (firstArg === undefined) {
+  console.log("No argument");
 } else {
-  console.log(argument);
+  console.log(firstArg);
 }
