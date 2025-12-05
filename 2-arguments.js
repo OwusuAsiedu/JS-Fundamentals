b8d458df64cf082fe6045df584a4810f37ff8208
OwusuAsiedu <nabcoerdig02594@gmail.com>
2025-12-05 11:37:49 +0000

    Add script that prints 3 lines

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,9 @@
-#!/usr/bin/node
-const args = process.argv.length;
+const argsCount = process.argv.length - 2;
 
-if (args <= 2) {
-  console.log('No argument');
-} else if (args === 3) {
-  console.log('Argument found');
+if (argsCount === 0) {
+  console.log("No argument");
+} else if (argsCount === 1) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');
+  console.log("Arguments found");
 }
